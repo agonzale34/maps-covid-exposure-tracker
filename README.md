11909126f078c01fbc72856edf7b9a81128f3dfd
Anthony Gonzalez <agonzale34@gmail.com>
2020-11-15 19:17:03 -0400

    Adding readme information
    - Improving the code output
    - updating the nova-scotia  exposures file

--- README.md
+++ README.md
@@ -1,6 +1,70 @@
-# Install and use conda Environment
+# Google Maps timeline Covid exposure tracker
+
+Python command line app that help you to get your Google maps timelines in .kml files and process them to validate
+possible Covid exposures based on preload exposure database.
+
+#### Install and use conda Environment
 
 ```bash
 conda env create -f environment.yml
 conda activate mcet
 ```
+
+## How to use:
+
+Run the command line app with:
+```bash
+python start.py
+```
+
+The app will ask a couple questions to set up the necessary information
+
+#### Choose how many days in the past you would like to check
+
+The default value will be 14 days but you can select any value grater that 1
+
+#### Get timelines from google maps
+
+If you want the app to help you downloading the timelines .kml files answer (y) yes. Otherwise the app will validate the
+files present in the `timelines` folder with filename like: `2020-11-15.kml`
+
+#### Get your google cookie:
+As google maps does not provide a dedicated API that we can use to get the timeline data, the only way is via cookie-jacking.
+
+In order to export processed data from Google Maps from this python script, you need to get your actual cookie.
+
+If you answered yes to the previous question the app will show a link that you can go to see your timeline of the current date
+
+When you are in the google maps website, open the developer tools in the Network tab.
+
+click in the google maps gear icon and click in Export this day to KML:
+
+![Download](images/maps-download-kml.png)
+
+When you download the file you will see a new request, right click in the one that start with `kml?` and copy as cURL
+
+![copy-cURL](images/maps-copy-cURL.png)
+
+Paste the cURL in a text editor and copy just the cookie content:
+```
+curl 'https://www.google.com/maps/timeline/kml?authuser=0&pb=!1m8!1m3!1i2017!2i3!3i16!2m3!1i2017!2i3!3i16'   
+-H 'accept-encoding: gzip, deflate, sdch, br'   
+-H 'accept-language: en-US,en;q=0.8,fr;q=0.6'   
+-H 'upgrade-insecure-requests: 1'   
+-H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) '   
+-H 'x-chrome-uma-enabled: 1'   
+-H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' 
+-H 'authority: www.google.com'   
+-H 'cookie: gsScrollPos=; _ga=GA1.1.49937635TxMcGmJ-uXXXXX gsScrollPos='  
+-H 'x-client-data: XXXQ==' --compressed 
+```
+
+Finally paste the cookie content in the app, if the cookie is valid, the app will download all the kml files inside 
+the `timelines` folder, process them and give you a list of possible exposures if any.
+
+![Results](images/result.png)
+
+#### Nova Scotia Covid Exposure database:
+
+the `exposures/nova-scotia.json` has the list with all the possible exposures from `2020-10-24`. I will try to update
+the file as much as possible. 
--- tracker.py
+++ tracker.py
@@ -4,7 +4,6 @@ import json
 from xml.dom import minidom
 
 import dateutil.parser as dp
-import pytz
 import requests
 import requests.cookies
 from dateutil import tz
@@ -12,6 +11,7 @@ from dateutil import tz
 MAPS_TODAY_URL = 'https://www.google.ca/maps/timeline?pli=1&pb=!1m2!1m1!1s{}-{}-{}'
 BASE_URL = 'https://www.google.ca/maps/timeline/kml?authuser=0&pb=!1m8!1m3!1i{0}!2i{1}!3i{2}!2m3!1i{0}!2i{1}!3i{2}'
 TIMELINE_FILE_TEMPLATE = 'timelines/{}-{}-{}.kml'
+EXPOSURE_FILE_TEMPLATE = 'exposures/{}.json'
 
 
 def get_map_url(year, month, day):
@@ -25,7 +25,6 @@ def print_line_separator():
 def download_maps_timeline(year, month, day, cookie_jar):
     r = requests.get(get_map_url(year, month, day), cookies=cookie_jar)
     if r.status_code == 200:
-        print("File downloaded successfully from google")
         file_name = TIMELINE_FILE_TEMPLATE.format(year, month, day)
         with open(file_name, 'w') as f:
             f.write(r.text)
@@ -36,9 +35,7 @@ def process_exposure_day(date, exposures):
     exposure_day = next((item for item in exposures['exposures'] if item['date'] == date.strftime('%Y-%m-%d')), None)
     possible_exposures = []
     if exposure_day is not None:
-        print_line_separator()
         default_date_time = dt.datetime(date.year, date.month, date.day, tzinfo=tz.gettz(exposures['timezone']))
-        print('Validating exposures for: ', date.strftime('%Y-%m-%d'))
         locations = exposure_day['locations']
         # open the kml file for this date
         timeline = minidom.parse(TIMELINE_FILE_TEMPLATE.format(date.year, date.month, date.day))
@@ -49,7 +46,6 @@ def process_exposure_day(date, exposures):
             if point:
                 place_name = place.getElementsByTagName('name')[0].firstChild.data
                 place_address = place.getElementsByTagName('address')[0].firstChild.data
-                print('Validating:', place_name, '-', place_address)
                 for location in locations:
                     if location['address'].lower() == place_address.lower():
                         # the address match, now we check the time frame:
@@ -57,17 +53,20 @@ def process_exposure_day(date, exposures):
                         begin = dp.parse(location['begin'], default=default_date_time)
                         end = dp.parse(location['end'], default=default_date_time)
                         begin_place = dp.parse(time_span.getElementsByTagName('begin')[0].firstChild.data)
-                        print(begin, end, begin_place)
                         if begin <= begin_place <= end:
-                            possible_exposures.append(location)
-                            print('ALERT: possible exposure found on:', place_name)
+                            possible_exposures.append({
+                                "name": place_name,
+                                "address": place_address,
+                                "start_time": begin_place
+                            })
 
-    return 0
+    return possible_exposures
 
 
 def run():
+    print_line_separator()
     print('Welcome to Maps COVID exposure tracker')
-
+    print_line_separator()
     days_past = input('How many days in that past you would like to check[14]: ')
     if days_past == '':
         days_past = 14
@@ -76,9 +75,7 @@ def run():
 
     init_date = dt.datetime.today() - dt.timedelta(days=days_past)
     final_date = dt.datetime.today()
-    print('Interval:', init_date.strftime('%Y-%m-%d'), final_date.strftime('%Y-%m-%d'))
-
-    exposure_file_path = 'exposures/nova-scotia.json'
+    print('Interval to check:', init_date.strftime('%Y-%m-%d'), final_date.strftime('%Y-%m-%d'))
 
     should_get_data = input('Would do you like to get the timeline data [Y/n]: ')
     if should_get_data.lower() == 'y':
@@ -95,11 +92,31 @@ def run():
             download_maps_timeline(date_to_get.year, date_to_get.month, date_to_get.day, cookie_jar)
         print('All days downloaded')
 
+    file_name = input('Please insert the exposure filename [nova-scotia]: ')
+    if file_name == '':
+        file_name = 'nova-scotia'
+    exposure_file_path = EXPOSURE_FILE_TEMPLATE.format(file_name)
+
+    print_line_separator()
+    print('Checking all the timelines against the database:', exposure_file_path)
     # open the exposures file
     with open(exposure_file_path, 'r') as read_file:
         exposures_data = json.load(read_file)
+        all_possible_exposures = []
         # Iterate the timelines and validate against the exposures
         for i in range(days_past):
             date_to_check = init_date + dt.timedelta(days=i)
-            process_exposure_day(date_to_check, exposures_data)
+            day_possible_exposures = process_exposure_day(date_to_check, exposures_data)
+            all_possible_exposures.extend(day_possible_exposures)
+
+        print_line_separator()
         print('All days has been validated!')
+        print_line_separator()
+
+        if len(all_possible_exposures) > 0:
+            print('ALERT: possible exposures found at:')
+            for p_expo in all_possible_exposures:
+                date_str = p_expo['start_time'].astimezone(tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
+                print('*', p_expo['name'], '- Time:', date_str)
+        else:
+            print('No possible exposure found at this time!')
